d1a7db22301a28c93da0819f9cad00cb3444337c
Matt Sell <mwsell@gmail.com>
2019-03-05 23:04:23 -0600

    init

---
+++ hooks/README.md
@@ -0,0 +1,7 @@
+# example
+
+## Install
+
+- In root of repo, run `yarn link`
+- In this folder, run `yarn link improving-mdx-theme`
+- Run `yarn start` to see theme
---
+++ hooks/deck.js
@@ -0,0 +1,25 @@
+import intro from './topics/intro.mdx'
+import wrapperHell from './topics/wrapper-hell.mdx'
+import overview from './topics/improving-overview.mdx'
+import newFeatures from './topics/new-features.mdx'
+import demo from './topics/demo.mdx'
+import qAndA from './topics/q-and-a.mdx'
+import conclusion from './topics/conclusion.mdx'
+
+// import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
+// import 'typeface-space-mono'
+// import 'typeface-spectral'
+
+export { default as theme } from 'improving-mdx-theme'
+export default [
+  ...intro,
+  ...wrapperHell,
+  ...overview,
+  ...newFeatures,
+  ...demo,
+  ...qAndA,
+  ...conclusion
+]
